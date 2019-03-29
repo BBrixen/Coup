ff44f6b2c332d4becff0ff1ab71d2ef071dce592
BBrixen <bbrixen@icloud.com>
2019-03-29 15:09:01 -0700

    .

--- src/DataTypes/Gamedata.java
+++ src/DataTypes/Gamedata.java
@@ -21,5 +21,67 @@ public class Gamedata extends Data {
         super(message, recipient, isGameData, isReturned, isUpdate);
     }
 
-    
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public static Server getServer() {
+        return server;
+    }
+
+    public static void setServer(Server server) {
+        Gamedata.server = server;
+    }
+
+    public static Deck getDeck() {
+        return deck;
+    }
+
+    public static void setDeck(Deck deck) {
+        Gamedata.deck = deck;
+    }
+
+    public static Scanner getScanner() {
+        return scanner;
+    }
+
+    public static void setScanner(Scanner scanner) {
+        Gamedata.scanner = scanner;
+    }
+
+    public static int[] getNumber_of_cards() {
+        return number_of_cards;
+    }
+
+    public static void setNumber_of_cards(int[] number_of_cards) {
+        Gamedata.number_of_cards = number_of_cards;
+    }
+
+    public static int getNumber_of_total_cards() {
+        return number_of_total_cards;
+    }
+
+    public static void setNumber_of_total_cards(int number_of_total_cards) {
+        Gamedata.number_of_total_cards = number_of_total_cards;
+    }
+
+    public static int getCards_per_player() {
+        return cards_per_player;
+    }
+
+    public static void setCards_per_player(int cards_per_player) {
+        Gamedata.cards_per_player = cards_per_player;
+    }
+
+    public static int getUniversal_i() {
+        return universal_i;
+    }
+
+    public static void setUniversal_i(int universal_i) {
+        Gamedata.universal_i = universal_i;
+    }
 }
